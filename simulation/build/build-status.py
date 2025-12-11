db5e6339b80e147e3630eac322100d3848ff9cb9
litmyp <lty1440249908@gmail.com>
2025-12-11 10:23:12 +0800

    add .gitignore

--- simulation/build/build-status.py
+++ simulation/build/build-status.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 
 # Programs that are runnable.
-ns3_runnable_programs = ['build/scratch/subdir/ns3.18-subdir-debug', 'build/scratch/ns3.18-scratch-simulator-debug', 'build/scratch/ns3.18-third_lty-debug', 'build/scratch/ns3.18-third-debug']
+ns3_runnable_programs = ['build/scratch/ns3.18-scratch-simulator-debug', 'build/scratch/subdir/ns3.18-subdir-debug', 'build/scratch/ns3.18-third-debug', 'build/scratch/ns3.18-third_lty-debug']
 
 # Scripts that are runnable.
 ns3_runnable_scripts = []
